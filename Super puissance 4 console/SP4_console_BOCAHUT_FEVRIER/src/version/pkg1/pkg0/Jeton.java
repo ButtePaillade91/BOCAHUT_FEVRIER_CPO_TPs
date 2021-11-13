9aa17c1d384f722e0a3f9be2e26b15e216b3d278
titou <titou@DESKTOP-AHI02ML.home>
2021-11-14 00:19:15 +0100

    voici notre classe jeton elle comporte: les attributs d'un jeton ainsi que ses méthodes

---
+++ Super puissance 4 console/SP4_console_BOCAHUT_FEVRIER/src/version/pkg1/pkg0/Jeton.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package version.pkg1.pkg0;
+
+/**
+ *
+ * @author titou
+ */
+public class Jeton {
+    String couleur;
+    
+    public Jeton(String couleurjeton) {
+        couleur = couleurjeton;
+    }
+    public String lireCouleur() {
+        return(couleur);
+    }
+}
